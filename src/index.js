811e0f15ff4824c7c0eab64ffa2a14e826240119
Matar <mattartheking@gmail.com>
2026-02-16 03:23:27 +0200

    added postman collection link in the index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import bootstrap from "./app.controller.js";
 
 
 bootstrap()
-
-
+//postman collection
+//https://mohamed-matar-uni-14-1097790.postman.co/workspace/Mohamed-Matar's-Workspace~fbe7bed6-edf9-4153-8bcc-a498d132b456/request/50803208-d7d38a9c-924f-4fd8-988c-9b5469856681?action=share&creator=50803208&ctx=documentation
 
